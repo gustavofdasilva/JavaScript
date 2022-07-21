72ceab37bfc0465cfbd49e17048e8ab915f0c5bf
Gustavo F. Silva <108156436+gustavofdasilva@users.noreply.github.com>
2022-07-21 16:27:59 -0300

    Eventos DOM e condições

---
+++ Projetos/Modulo-D/E008.js
@@ -0,0 +1,18 @@
+//* Condição simples
+    var vel = 6;
+    console.log(`A velocidade do carro é ${vel} km/h`)
+    if(vel >= 60) {
+        console.log("Aviso! Utilize sempre o cinto de segurança");
+    }
+    
+
+//?
+
+//* Condição composta
+    var moradia = "França"
+    console.log(`Você está vivendo no ${moradia}`)
+    if(moradia == "Brasil") {
+        console.log("Olá brasileiro!");
+    } else if(moradia != "Brasil") {
+        console.log("Olá Estrangeiro!");
+    }
