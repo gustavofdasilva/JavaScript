c13752fb49c99c5d2647f640e2940f77ca352b72
Gustavo F. Silva <108156436+gustavofdasilva@users.noreply.github.com>
2022-07-26 17:08:53 -0300

    Exercicios e repetições
    Rever exercicio de contador, com erro bizarro

---
+++ Projetos/Modulo-E/ambiente.js
@@ -0,0 +1,23 @@
+
+/*
+
+var n = 0
+do {
+    console.log("Tudo bem?");
+    n++;
+} while (n<100)
+
+
+//*
+
+var n = 0
+while (n<100) {
+    console.log(`Tudo bem? ${n}`);
+    n++;
+} */
+
+console.log("Vai começar!");
+for(var n = 0 ; n<100 ; n++) {
+    console.log("Tudobem?" + n);
+}
+console.log("FIM!");
