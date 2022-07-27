2cda85eea562e396a316db7c9bde6f0abad0aee1
Gustavo F. Silva <108156436+gustavofdasilva@users.noreply.github.com>
2022-07-27 15:56:10 -0300

    Revisando exercícios

---
+++ Projetos/Modulo-E/EX/script.js
@@ -0,0 +1,20 @@
+var area = document.querySelector("select");
+var num = document.getElementById("num");
+var EachRes
+
+function calcular() {
+    area.innerHTML = "";
+    if(num.value == ""){
+        alert("Impossível calcular!");
+    } else {
+        for(var i = 1; i <= 10 ; i++) {
+            EachRes = Number(num.value*i);
+            area.innerHTML += 
+            `
+            <option>
+            ${num.value} x ${i} = ${EachRes}
+            </option>
+            `
+        }
+    }
+}
