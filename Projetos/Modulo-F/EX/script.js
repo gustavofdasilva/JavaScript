4009755e6e9dcf56e6bd457f7d93331f9e9b064b
Gustavo F. Silva <108156436+gustavofdasilva@users.noreply.github.com>
2022-07-29 17:17:51 -0300

    projeto final!

---
+++ Projetos/Modulo-F/EX/script.js
@@ -0,0 +1,67 @@
+var dados = []
+var area = document.querySelector("select");
+var areaResult = document.getElementById("AreaR");
+var counter = 0;
+
+
+function getNumber() {
+    
+    area = document.querySelector("select");
+    var Enum = document.getElementById("num");
+    var num = Number(Enum.value);
+
+    if(num>=1 && num<=100) {
+        areaResult.innerHTML="";
+        dados.push(num);
+        
+        area.innerHTML += `
+        <option>
+        
+            Valor ${num} adicionado!
+    
+        </option>`  
+        counter++;  
+    } else {
+        window.alert("dados inválidos!");
+    }
+}
+
+function result() {
+    if(counter>0) {
+        var max = 0
+        var min = max
+        var soma = 0
+        var media = 0
+
+        for(var i in dados) {
+            if(max < dados[i]) {
+                max = dados[i]
+            }
+        }
+
+        min = max
+        for(var i in dados) {
+            if(min>dados[i]) {
+                min = dados[i]
+            }
+        }
+
+        for(var i in dados) {
+            soma += dados[i];
+            media = soma/dados.length;
+        }
+
+        
+        areaResult.innerHTML = `
+        
+        Ao todo, temos ${dados.length} valores cadastrados <br>
+        O maior valor foi o ${max} <br>
+        O menor valor foi o ${min} <br>
+        A soma dos valores foi ${soma} <br>
+        A média dos valores foi ${media}
+        
+        `
+    } else {
+        window.alert("Insira os valores!");
+    }
+}
--- Projetos/Modulo-F/ambiente.js
+++ Projetos/Modulo-F/ambiente.js
@@ -1,4 +1,4 @@
-var num = [5 , 3 , 4 , 3 , 6 , 6 , 3 , 2 , 9 , 6 , 6 , 4 , 5 , 1]
+/*var num = [5 , 3 , 4 , 3 , 6 , 6 , 3 , 2 , 9 , 6 , 6 , 4 , 5 , 1]
 num[5] = 9
 num.push(7) //Coloca ao final do array
 
@@ -11,6 +11,18 @@ for (var n = 0 ; n <= num.length ; n++) {
 for (var n in num) {
     console.log(num[n]);
 }
-*/
+
 console.log(num.indexOf(6));
 console.log(num.indexOf(14));
+*/
+
+//* RECURSIVIDADE  
+function fatorial(n) {
+    if(n==1) {
+        return 1;
+    } else {
+        return n * fatorial(n-1);
+    }
+}
+
+console.log(fatorial(5))
